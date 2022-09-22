bb30d5142b1677844811319a421e1980a4bc8c5a
Colton Fox <coltonfox@coltons-mbp.wifi.uc.edu>
2022-09-22 15:14:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int numA = 5;
+        int numB = 5;
+
+        if(numA == numB)
+        {
+            System.out.println("Your two numbers are equal");
+        }
+        else if(numA < numB)
+        {
+            System.out.println("numA is less than numB");
+        }
+        else
+        {
+            System.out.println("numB is less than numA ");
+
+        }
+    }
+}
